7aeca72ec9ae083afd9a0d6e18d0759526d717bc
christinaread <106094278+christinaread@users.noreply.github.com>
2022-07-01 10:44:05 +0200

    Add files via upload

---
+++ extras/CodeToRun.R
@@ -0,0 +1,59 @@
+library(EHDENUseCaseNICE)
+
+# Optional: specify where the temporary files (used by the Andromeda package) will be created:
+options(andromedaTempFolder = "s:/andromedaTemp")
+
+# Maximum number of cores to be used:
+maxCores <- parallel::detectCores()
+
+# The folder where the study intermediate and result files will be written:
+outputFolder <- "s:/EHDENUseCaseNICE"
+
+# Details for connecting to the server:
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "pdw",
+                                                                server = Sys.getenv("PDW_SERVER"),
+                                                                user = NULL,
+                                                                password = NULL,
+                                                                port = Sys.getenv("PDW_PORT"))
+
+# The name of the database schema where the CDM data can be found:
+cdmDatabaseSchema <- "CDM_IBM_MDCD_V1153.dbo"
+
+# The name of the database schema and table where the study-specific cohorts will be instantiated:
+cohortDatabaseSchema <- "scratch.dbo"
+cohortTable <- "mschuemi_skeleton"
+
+# Some meta-information that will be used by the export function:
+databaseId <- "Synpuf"
+databaseName <- "Medicare Claims Synthetic Public Use Files (SynPUFs)"
+databaseDescription <- "Medicare Claims Synthetic Public Use Files (SynPUFs) were created to allow interested parties to gain familiarity using Medicare claims data while protecting beneficiary privacy. These files are intended to promote development of software and applications that utilize files in this format, train researchers on the use and complexities of Centers for Medicare and Medicaid Services (CMS) claims, and support safe data mining innovations. The SynPUFs were created by combining randomized information from multiple unique beneficiaries and changing variable values. This randomization and combining of beneficiary information ensures privacy of health information."
+
+# For Oracle: define a schema that can be used to emulate temp tables:
+oracleTempSchema <- NULL
+
+execute(connectionDetails = connectionDetails,
+        cdmDatabaseSchema = cdmDatabaseSchema,
+        cohortDatabaseSchema = cohortDatabaseSchema,
+        cohortTable = cohortTable,
+        oracleTempSchema = oracleTempSchema,
+        outputFolder = outputFolder,
+        databaseId = databaseId,
+        databaseName = databaseName,
+        databaseDescription = databaseDescription,
+        createCohorts = TRUE,
+        synthesizePositiveControls = TRUE,
+        runAnalyses = TRUE,
+        packageResults = TRUE,
+        maxCores = maxCores)
+
+resultsZipFile <- file.path(outputFolder, "export", paste0("Results_", databaseId, ".zip"))
+dataFolder <- file.path(outputFolder, "shinyData")
+
+# You can inspect the results if you want:
+prepareForEvidenceExplorer(resultsZipFile = resultsZipFile, dataFolder = dataFolder)
+launchEvidenceExplorer(dataFolder = dataFolder, blind = TRUE, launch.browser = FALSE)
+
+# Upload the results to the OHDSI SFTP server:
+privateKeyFileName <- ""
+userName <- ""
+uploadResults(outputFolder, privateKeyFileName, userName)
