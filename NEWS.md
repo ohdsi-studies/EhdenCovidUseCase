c285c5c974cad720951999db502ee2967b87ab19
christinaread <106094278+christinaread@users.noreply.github.com>
2022-10-12 11:01:15 +0200

    Add files via upload

---
+++ NEWS.md
@@ -0,0 +1,27 @@
+(Note: currently not using version numbers, so referencing release time instead)
+
+EHDENUseCase5.3CCISex Q1 2022
+======================================
+
+Changes:
+
+1. Using `CohortGenerator` package to generate cohorts. This does mean several other tables will be created to hold cohort creation statistics.
+
+2. Checking whether `tempEmulationSchema` is provided for those DBMSs that need it.
+
+3. Checking if `andromedaTempFolder` can be created.
+
+4. Fetching vocabulary version, study package version, and observation period range when exporting database meta-data.
+
+5. Using new `ParallelLogger`, so changing all `ParallelLogger::logInfo()` to `message()` for prettier code.
+
+6. Normalizing all paths, so there should be no issue when relative paths are used.
+
+7. Using `keyring` in example code.
+
+8. Updating `renv.lock` to latest versions of all dependencies.
+
+9. Storing cohort definitions according to new standard: JSON and SQL file names derived from cohort IDs. List of cohorts in `Cohorts.csv`, with columns `webApiCohortId`, `cohortId` and `cohortName`.
+
+
+
---
+++ extras/CodeToRun.R
@@ -0,0 +1,58 @@
+library(EHDENUseCase5.3CCISex)
+
+# Optional: specify where the temporary files (used by the Andromeda package) will be created:
+options(andromedaTempFolder = "d:/andromedaTemp")
+
+# Maximum number of cores to be used:
+maxCores <- parallel::detectCores() - 1
+
+# The folder where the study intermediate and result files will be written:
+outputFolder <- "d:/EHDENUseCase5.3CCISex"
+
+# Details for connecting to the server:
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "redshift",
+                                                                connectionString = keyring::key_get("redShiftConnectionStringOhdaMdcr"),
+                                                                user = keyring::key_get("redShiftUserName"),
+                                                                password = keyring::key_get("redShiftPassword"))
+
+# The name of the database schema where the CDM data can be found:
+cdmDatabaseSchema <- "cdm_truven_mdcr_v1911"
+
+# The name of the database schema and table where the study-specific cohorts will be instantiated:
+cohortDatabaseSchema <- "scratch_mschuemi"
+cohortTable <- "estimation_skeleton"
+
+# Some meta-information that will be used by the export function:
+databaseId <- "IBM_MDCR"
+databaseName <- "IBM MarketScan® Medicare Supplemental and Coordination of Benefits Database"
+databaseDescription <- "IBM MarketScan® Medicare Supplemental and Coordination of Benefits Database (MDCR) represents health services of retirees in the United States with primary or Medicare supplemental coverage through privately insured fee-for-service, point-of-service, or capitated health plans.  These data include adjudicated health insurance claims (e.g. inpatient, outpatient, and outpatient pharmacy). Additionally, it captures laboratory tests for a subset of the covered lives."
+
+# For some database platforms (e.g. Oracle): define a schema that can be used to emulate temp tables:
+options(sqlRenderTempEmulationSchema = NULL)
+
+execute(connectionDetails = connectionDetails,
+        cdmDatabaseSchema = cdmDatabaseSchema,
+        cohortDatabaseSchema = cohortDatabaseSchema,
+        cohortTable = cohortTable,
+        outputFolder = outputFolder,
+        databaseId = databaseId,
+        databaseName = databaseName,
+        databaseDescription = databaseDescription,
+        verifyDependencies = TRUE,
+        createCohorts = TRUE,
+        synthesizePositiveControls = TRUE,
+        runAnalyses = TRUE,
+        packageResults = TRUE,
+        maxCores = maxCores)
+
+resultsZipFile <- file.path(outputFolder, "export", paste0("Results_", databaseId, ".zip"))
+dataFolder <- file.path(outputFolder, "shinyData")
+
+# You can inspect the results if you want:
+prepareForEvidenceExplorer(resultsZipFile = resultsZipFile, dataFolder = dataFolder)
+launchEvidenceExplorer(dataFolder = dataFolder, blind = TRUE, launch.browser = FALSE)
+
+# Upload the results to the OHDSI SFTP server:
+privateKeyFileName <- ""
+userName <- ""
+uploadResults(outputFolder, privateKeyFileName, userName)
